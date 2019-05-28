b032176b75fb1c74eb596fcc983890d172ebfece
unset <unset>
2019-05-28 17:34:17 -0300

    adicionados aulaJava

---
+++ luiz-java/src/Luiz.java
@@ -0,0 +1,44 @@
+import java.util.Scanner;
+public class Luiz {
+
+    public static void main(String[] args) {
+//        teste
+//        float n1, n2, media;
+//
+//        Scanner dado = new Scanner(System.in);
+//        System.out.println("Digite a Nota1: ");
+//        n1 = dado.nextFloat();
+//
+//        System.out.println("digite a Nota2: ");
+//        n2 = dado.nextFloat();
+//
+//        media = (n1+n2)/2;
+//
+//        System.out.println("media: " + media);
+
+//          EX1
+//        float n1, n2,n3,n4,media;
+//
+//        Scanner dado = new Scanner(System.in);
+//        System.out.println("Digite a Nota1: ");
+//        n1 = dado.nextFloat();
+//
+//        System.out.println("digite a Nota2: ");
+//        n2 = dado.nextFloat();
+//
+//        System.out.println("digite a Nota3: ");
+//        n3 = dado.nextFloat();
+//
+//        System.out.println("digite a Nota4: ");
+//        n4 = dado.nextFloat();
+//
+//        media = (n1+n2+n3+n4)/4;
+//
+//        System.out.println("media: " + media);
+
+
+//          ex2
+        
+    }
+}
+
