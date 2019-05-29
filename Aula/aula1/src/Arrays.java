c079f2413fe048a3c61b19a921905f17ed1e7090
viborotto <vittoriaborotto@gmail.com>
2019-05-29 17:45:22 -0300

    ultimo sav do dia

---
+++ Aula/aula1/src/Arrays.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+public class Arrays {
+    public static void main(String[] arg) {
+        Scanner arrays = new Scanner(System.in);
+        int numUser;
+        System.out.println("digigte um numero:");
+        numUser = arrays.nextInt();
+        boolean usuario = true;
+
+        for (int i = 2; i < numUser; i++) {
+            
+        }
+
+
+
+        }
+    }
+
---
+++ Aula/aula1/src/Loops.java
@@ -0,0 +1,36 @@
+public class Loops {
+    public static void main(String[] arg) {
+
+        //Ex: 1
+
+        /*  for (float ano = 1995; ano<2020; ano++);
+        float s = 1000;
+        float sa = 1000 + 1.5%;
+        float au = 1.5%;
+        float na = au * 2; */
+
+
+        // Ex: 4
+
+        /*
+        int soma = 0;
+        for (int i = 0; i < 101; i += 2) {
+         soma = soma + i;
+          System.out.println(soma);
+        }
+         */
+
+
+        // Ex: 9
+            /* int soma = 0;
+            for (int i = 0; i <= 100; i += 2){
+                soma = soma + i;
+                System.out.println(soma);
+            }  */
+
+
+
+
+         // Ex: 10
+    }
+}
